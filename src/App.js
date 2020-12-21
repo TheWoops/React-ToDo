da15a9b164cc0e6d149898ec4d9ca584da2d79a1
TheWoops <thewoops.deeplearning@gmail.com>
2020-12-21 15:43:07 +0100

    finished todo-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import { nanoid } from "nanoid";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
-function App() {
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+
+  const [filter, setFilter] = useState('All');
+  const listHeadingRef = useRef(null);
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+     key={name}
+     name={name}
+     setFilter = {setFilter}
+     isPressed = {name === filter}
+    />
+  ));
+
+  function toggleTaskCompleted(id) {
+
+    const updatedTasks = tasks.map(task => {
+
+      if (task.id === id){
+        return {...task, completed : !task.completed}
+      }
+      return task
+    } );
+    setTasks(updatedTasks);
+  }
+
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task =>
+     <Todo 
+     id = {task.id}
+     name = {task.name} 
+     completed = {task.completed}
+     key = {task.id}
+     toggleTaskCompleted={toggleTaskCompleted}
+     deleteTask = {deleteTask} 
+     editTask = {editTask}/>);
+  const taskNoun = taskList.length !==1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
+
+  function addTask(name){
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+
+  const prevTaskLength = usePrevious(tasks.length);
+
+  
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form onSubmit = {addTask}/>
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading" tabIndex = "-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+
+      {taskList}
+      </ul>
     </div>
   );
 }
